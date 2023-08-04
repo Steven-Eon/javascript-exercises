9a84334dc73de51c371d0a4d4e0717409c3c3fc4
Steven-Eon <91300742+Steven-Eon@users.noreply.github.com>
2023-08-03 22:16:43 -0700

    Completed Exercise 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let finalString = "";
+    for (let i = 0; i < stringToReverse.length; ++i)
+    {
+        finalString += stringToReverse[stringToReverse.length - 1 - i];
+    }
+    return finalString;
 };
 
 // Do not edit below this line

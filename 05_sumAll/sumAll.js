547b80d3bef53225e0d12f417564ded689dbefdb
Steven-Eon <91300742+Steven-Eon@users.noreply.github.com>
2023-08-03 22:51:41 -0700

    Exercise 5 completed!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    finalSum = 0;
+    
+    if (typeof(num1) != "number" || typeof(num2) != "number" || num1 < 0 || num2 < 0)
+    {
+        return "ERROR";
+    }
 
+    if (num1 <= num2) 
+    {
+        for (let i = num1; i < num2 + 1; ++i)
+        {
+            finalSum += i;
+        }
+    }
+    else
+    {
+        for (let i = num2; i < num1 + 1; ++i)
+        {
+            finalSum += i;
+        }
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line

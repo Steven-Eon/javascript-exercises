2e53cdef0358d62c9b751513b316e6591678adfd
Steven-Eon <91300742+Steven-Eon@users.noreply.github.com>
2023-08-03 22:41:17 -0700

    Exercise 4 completed.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToRemove, ...numbersToRemove) {
+    let numsToRemove = numbersToRemove;
+    let finalArray = arrayToRemove;
 
+    for (let i = 0; i < finalArray.length; ++i)
+    {
+        for (let j = 0; j < numsToRemove.length; ++j)
+        {
+            if (finalArray[i] === numsToRemove[j])
+            {
+                finalArray.splice(i, 1);
+                --i;
+            }
+        }
+    }
+    return finalArray;
 };
 
 // Do not edit below this line

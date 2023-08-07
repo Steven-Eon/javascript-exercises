f7e5b1800b90cc666157a3d50af71cbc0ad70d5d
Steven-Eon <91300742+Steven-Eon@users.noreply.github.com>
2023-08-07 00:26:07 -0700

    Exercise 9 completed!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const filteredString = Array.from(string).filter((letter) => {
+        const numToCheck = parseInt(letter.charCodeAt(0));
+        if ((numToCheck >= 48 && numToCheck <= 57) || (numToCheck >= 65 && numToCheck <= 90) || (numToCheck >= 97 && numToCheck <= 122))
+        {
+            return true;
+        }
+    })
+    
+    for (let i = 0; i < filteredString.length / 2; ++i)
+    {
+        if (filteredString.at(i).toLowerCase() != filteredString.at(filteredString.length - 1 - i).toLowerCase())
+        {
+            return false;
+        }
+    }
 
+    return true;
 };
 
 // Do not edit below this line

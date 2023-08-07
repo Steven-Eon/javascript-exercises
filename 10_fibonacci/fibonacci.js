cc04c8fb20d24f3b6dd28aa7cdb9c9358d53f8de
Steven-Eon <91300742+Steven-Eon@users.noreply.github.com>
2023-08-07 00:55:41 -0700

    Exercise 10 completed!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(iterations) {
+    const numOfIterations = Number.parseInt(iterations);
+    if (numOfIterations < 0)
+    {
+        return "OOPS";
+    }
+    
+    let array = [1, 1]
+    for (let i = 2; i < numOfIterations; ++i)
+    {
+        array.push(array[i - 1] + array[i - 2]);
+    }
 
+    return array[numOfIterations - 1];
 };
 
 // Do not edit below this line

ab4716533f3aaf64c2c9675d8885abc78a4e22b6
Steven-Eon <91300742+Steven-Eon@users.noreply.github.com>
2023-08-07 01:11:45 -0700

    Exercise 12 completed!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayToSearch) {
+    const oldest = arrayToSearch.reduce((currOldest, currElement) =>
+    {
+        const date = new Date()
+        if (currElement.yearOfDeath === undefined || currOldest.yearOfDeath === undefined)
+        {
+            currOldest = date.getFullYear() - currElement.yearOfBirth > date.getFullYear() - currOldest.yearOfBirth ? currElement : currOldest;
+        }
+        else if (currElement.yearOfDeath === undefined)
+        {
+            currOldest = date.getFullYear() - currElement.yearOfBirth > currOldest.yearOfDeath - currOldest.yearOfBirth ? currElement : currOldest;
+        }
+        else if (currOldest.yearOfDeath === undefined)
+        {
+            currOldest = currElement.yearOfDeath - currElement.yearOfBirth > date.getFullYear() - currOldest.yearOfBirth ? currElement : currOldest;
+        }
+        else
+        {
+            currOldest = currElement.yearOfDeath - currElement.yearOfBirth > currOldest.yearOfDeath - currOldest.yearOfBirth ? currElement : currOldest;
+        }
 
+        return currOldest
+    }, arrayToSearch[0]);
+
+
+    console.log(oldest.name);
+    return oldest;
 };
 
 // Do not edit below this line
